a8e6431d4801c3eca0c65bf84ce2172747d5cec7
gabriel <gabrielbsnsp@gmail.com>
2022-07-09 13:40:45 -0300

    first commit

---
+++ js/database.js
@@ -0,0 +1,40 @@
+let produtos = [
+    {
+        nome: 'Item',
+        preco: 'Valor'
+    },
+    {
+        nome: 'Uva Crimson',
+        preco: 44.99
+    },
+    {
+        nome: 'Vinho Canção',
+        preco: 17.98
+    },
+    {
+        nome: 'Água de coco',
+        preco: 8.99
+    },
+    {
+        nome: 'Mamão',
+        preco: 9.98
+    },
+    {
+        nome: 'Água tônica',
+        preco: 17.98
+    },
+    {
+        nome: 'Milho Verde',
+        preco: 10.95
+    }
+]
+
+function newProduct(){
+    let object = {
+        nome: prompt("produto"),
+        preco: parseFloat(prompt('valor'))
+    }
+    produtos.push(object)
+}
+
+
--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//Função que cria o html base da minha aplicação
+
 function createBasicHtml() {
     //Criação das tag: Main
     let body = document.querySelector("body")
@@ -8,25 +10,29 @@ function createBasicHtml() {
     let header = document.createElement("header")
     main.appendChild(header)
 
-    //Criação da tag: h1
+    //Criação da tag: img
     let img2 = document.createElement("img")
-    img2.src = './img/walking.gif'
+    img2.src = '../img/walking.gif'
     img2.alt = 'man walking market'
     header.appendChild(img2)
 
+    //Criação da tag: h1
     let h1 = document.createElement("h1")
     h1.innerText = (`Virtual Market`)
     header.appendChild(h1)
 
+    //Criação da tag: img
     let img = document.createElement("img")
-    img.src = './img/market.png'
+    img.src = '../img/market.png'
     img.alt = 'market image'
     header.appendChild(img)
 
+    //Criação da tag: section
     let section = document.createElement("section")
     section.classList.add("container")
     main.appendChild(section)
 
+    //Criação da tag: ul
     let ul = document.createElement("ul")
     ul.classList.add("productList")
     section.appendChild(ul)
@@ -34,9 +40,11 @@ function createBasicHtml() {
 }
 createBasicHtml()
 
-
+//Array para armazenar os valores em reais.
 let price = []
 
+
+//Função que pega cada item do meu "Banco de dados" individualmente.
 function item(array) {
     for(i = 0; i < array.length; i++) {
         let select = document.querySelector(".productList")
@@ -49,6 +57,7 @@ item(produtos)
 
 
 
+// Função que pega os itens do banco de dados individualmente e os trata, criando uma sessão de itens no html.
 function createItem(produto) {
     price.push(produto.preco)
     let li = document.createElement("li")
@@ -66,10 +75,15 @@ function createItem(produto) {
 }
 
 
+
+// Tratamento dos valores para efetuar a soma total
 price.shift()
 const somarValores = price => ( price.reduce( (value,nextValue) => value + nextValue ) )
 let resultadoSoma = somarValores(price).toFixed(2)
 
+
+
+// Função que cria meu "Resultado total" no html.
 function totalHtml(resultado) {
     let section = document.querySelector(".container")
 
@@ -91,6 +105,9 @@ function totalHtml(resultado) {
     return section.appendChild(ul)
 }
 
+
+
+// Função que cria minha sessão " Finalizar Compra " no html.
 function createButton() {
     let select = document.querySelector(".productDetails")
 
